d158cfa9c4bc42c8806e0dfb5fbd6fd20fffc205
Новиков Алексей Юрьевич <novikov_ay@altravita.local>
2024-05-29 15:37:37 +0300

    submitPostQuerry31andLoader5

--- src/components/colChatsGpt/colChatsGpt.jsx
+++ src/components/colChatsGpt/colChatsGpt.jsx
@@ -133,6 +133,7 @@ const ColChatsGpt = (props) => {
                         props.changeSessionForSubmit === true &&
                         <span className="loader2"></span>
                     }
+                    <span className="loader2"></span>
                 </div>
             </div>
             <form action="" id="formChatGpt">
