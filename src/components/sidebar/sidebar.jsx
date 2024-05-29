3ecf03b243c0276de15d8a3d9ae51dd62f6d233b
Новиков Алексей Юрьевич <novikov_ay@altravita.local>
2024-05-29 11:27:17 +0300

    submitSessionAsync7

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -55,7 +55,7 @@ const Sidebar = (props) => {
 
             }      //   отправка запроса при нажатии на название сессии
         submitSession();
-    }, [props, llm_session_title, llm_session_id, props.llm_session_title, props.llm_session_id]);
+    }, [llm_session_title, llm_session_id, props.llm_session_title, props.llm_session_id]);
 
 
 
