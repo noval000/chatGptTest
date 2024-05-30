a8d82749ed4683923f8333f23d68af719083545f
Новиков Алексей Юрьевич <novikov_ay@altravita.local>
2024-05-30 14:25:45 +0300

    loaderNewForms44

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -31,83 +31,7 @@ const Home = (props) => {
 
 
 
-    // useEffect(() => {
-    //     const submitFirstChat = (e) => {
-    //         // e.preventDefault();
-    //         // Данные для отправки на сервер
-    //         const data = {
-    //             llm_session_title,
-    //             llm_session_id,
-    //             inpGetValue,
-    //             valueChatGpt,
-    //             valueChatClaude,
-    //             valueChatGigachat
-    //         };
-    //         // Отправка данных на сервер
-    //         axios.post('/api/llm_session/new_query', data)
-    //             .then(response => {
-    //                 console.log('Server response:', response.data);
-    //                 const messageGpt = response.data.filter(el => el.datetime_response === 'chatgpt')
-    //                 const messageClaude = response.data.filter(el => el.datetime_response === 'claude')
-    //                 const messageGigachat = response.data.filter(el => el.datetime_response === 'chatgpt')
-    //                 const newMessageGpt = {
-    //                     datetime_query: messageGpt[0].datetime_query,
-    //                     datetime_response: messageGpt[0].datetime_response,
-    //                     id: messageGpt[0].id,
-    //                     model: messageGpt[0].model,
-    //                     query: messageGpt[0].query,
-    //                     response: messageGpt[0].response,
-    //                     session_id: messageGpt[0].session_id,
-    //                     task: messageGpt[0].task,
-    //                     user_score: messageGpt[0].user_score
-    //                 }
-    //                 const newMessageClaude = {
-    //                     datetime_query: messageClaude[0].datetime_query,
-    //                     datetime_response: messageClaude[0].datetime_response,
-    //                     id: messageClaude[0].id,
-    //                     model: messageClaude[0].model,
-    //                     query: messageClaude[0].query,
-    //                     response: messageClaude[0].response,
-    //                     session_id: messageClaude[0].session_id,
-    //                     task: messageClaude[0].task,
-    //                     user_score: messageClaude[0].user_score
-    //                 }
-    //                 const newMessageGigachat = {
-    //                     datetime_query: messageGigachat[0].datetime_query,
-    //                     datetime_response: messageGigachat[0].datetime_response,
-    //                     id: messageGigachat[0].id,
-    //                     model: messageGigachat[0].model,
-    //                     query: messageGigachat[0].query,
-    //                     response: messageGigachat[0].response,
-    //                     session_id: messageGigachat[0].session_id,
-    //                     task: messageGigachat[0].task,
-    //                     user_score: messageGigachat[0].user_score
-    //                 }
-    //                 setSessionChatGpt([...sessionChatGpt, newMessageGpt])
-    //                 setSessionChatGigachat([...sessionChatGigachat, newMessageGigachat])
-    //                 setSessionChatClaude([...sessionChatClaude, newMessageClaude])
-    //             })
-    //             .finally(() => {
-    //                 setValueChatGpt('');
-    //                 setValueChatGigachat('');
-    //                 setValueChatClaude('');
-    //                 setChangeSessionForSubmit(false)
-    //             })
-    //             .catch(error => {
-    //                 console.error('There was an error sending the data!', error);
-    //             });
-    //     };
-    //     submitFirstChat();
-    //     // if (changeSessionForSubmit === true) {
-    //     //     setTimeout(() => {
-    //     //         setChangeSessionForSubmit(false)
-    //     //     }, 1500);
-    //     // }
-    //     console.log(changeSessionForSubmit)
-    //     console.log(sessionChatGpt)
-    // }, [changeSessionForSubmit]);    //  отправка запроса на ответ
-
-
+    useEffect(() => {
         const submitFirstChat = (e) => {
             // e.preventDefault();
             // Данные для отправки на сервер
@@ -125,7 +49,7 @@ const Home = (props) => {
                     console.log('Server response:', response.data);
                     const messageGpt = response.data.filter(el => el.datetime_response === 'chatgpt')
                     const messageClaude = response.data.filter(el => el.datetime_response === 'claude')
-                    const messageGigachat = response.data.filter(el => el.datetime_response === 'chatgpt')
+                    const messageGigachat = response.data.filter(el => el.datetime_response === 'gigachat')
                     const newMessageGpt = {
                         datetime_query: messageGpt[0].datetime_query,
                         datetime_response: messageGpt[0].datetime_response,
@@ -173,15 +97,16 @@ const Home = (props) => {
                     console.error('There was an error sending the data!', error);
                 });
         };
-        useEffect(() => {
-            submitFirstChat();
-        }, [changeSessionForSubmit])
-
+        submitFirstChat();
         // if (changeSessionForSubmit === true) {
         //     setTimeout(() => {
         //         setChangeSessionForSubmit(false)
         //     }, 1500);
         // }
+        console.log(changeSessionForSubmit)
+        console.log(sessionChatGpt)
+    }, [changeSessionForSubmit]);    //  отправка запроса на ответ
+
 
 
 
