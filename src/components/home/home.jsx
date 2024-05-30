5672594b5d2e23875a07b11aa13f8724cd9f6001
Новиков Алексей Юрьевич <novikov_ay@altravita.local>
2024-05-30 17:13:33 +0300

    loaderNewForms74

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -94,6 +94,7 @@ const Home = (props) => {
                     setSessionChatGigachat([...sessionChatGigachat, newMessageGigachat])
                 })
                 .finally(() => {
+                    setValue('');
                     // setValueChatGpt('');
                     setValueChatGigachat('');
                     // setValueChatClaude('');
