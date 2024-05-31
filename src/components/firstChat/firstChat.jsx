770a6a7e689538889761bf3f3e7b2952a17c1921
Новиков Алексей Юрьевич <novikov_ay@altravita.local>
2024-05-31 15:51:19 +0300

    new session67

--- src/components/firstChat/firstChat.jsx
+++ src/components/firstChat/firstChat.jsx
@@ -50,8 +50,8 @@ const FirstChat = (props) => {
                                                console.log('Server response:', response.data);
                                                // const messageGpt = response.data.filter(el => el.datetime_response === 'chatgpt');
                                                const messageGpt = response.data.llm_responses.filter(el => el.datetime_response === 'chatgpt');
-                                               const messageClaude = response.data.filter(el => el.datetime_response === 'claude');
-                                               const messageGigachat = response.data.filter(el => el.datetime_response === 'gigachat');
+                                               // const messageClaude = response.data.filter(el => el.datetime_response === 'claude');
+                                               // const messageGigachat = response.data.filter(el => el.datetime_response === 'gigachat');
                                                const newMessageGpt = {
                                                    datetime_query: messageGpt.datetime_query,
                                                    datetime_response: messageGpt.datetime_response,
@@ -63,31 +63,31 @@ const FirstChat = (props) => {
                                                    task: messageGpt.task,
                                                    user_score: messageGpt.user_score
                                                };
-                                               const newMessageClaude = {
-                                                   datetime_query: messageClaude[0].datetime_query,
-                                                   datetime_response: messageClaude[0].datetime_response,
-                                                   id: messageClaude[0].id,
-                                                   model: messageClaude[0].model,
-                                                   query: inpGetValue,
-                                                   response: messageClaude[0].response,
-                                                   session_id: messageClaude[0].session_id,
-                                                   task: messageClaude[0].task,
-                                                   user_score: messageClaude[0].user_score
-                                               };
-                                               const newMessageGigachat = {
-                                                   datetime_query: messageGigachat[0].datetime_query,
-                                                   datetime_response: messageGigachat[0].datetime_response,
-                                                   id: messageGigachat[0].id,
-                                                   model: messageGigachat[0].model,
-                                                   query: inpGetValue,
-                                                   response: messageGigachat[0].response,
-                                                   session_id: messageGigachat[0].session_id,
-                                                   task: messageGigachat[0].task,
-                                                   user_score: messageGigachat[0].user_score
-                                               };
+                                               // const newMessageClaude = {
+                                               //     datetime_query: messageClaude[0].datetime_query,
+                                               //     datetime_response: messageClaude[0].datetime_response,
+                                               //     id: messageClaude[0].id,
+                                               //     model: messageClaude[0].model,
+                                               //     query: inpGetValue,
+                                               //     response: messageClaude[0].response,
+                                               //     session_id: messageClaude[0].session_id,
+                                               //     task: messageClaude[0].task,
+                                               //     user_score: messageClaude[0].user_score
+                                               // };
+                                               // const newMessageGigachat = {
+                                               //     datetime_query: messageGigachat[0].datetime_query,
+                                               //     datetime_response: messageGigachat[0].datetime_response,
+                                               //     id: messageGigachat[0].id,
+                                               //     model: messageGigachat[0].model,
+                                               //     query: inpGetValue,
+                                               //     response: messageGigachat[0].response,
+                                               //     session_id: messageGigachat[0].session_id,
+                                               //     task: messageGigachat[0].task,
+                                               //     user_score: messageGigachat[0].user_score
+                                               // };
                                                props.setSessionChatGpt([...props.sessionChatGpt, newMessageGpt]);
-                                               props.setSessionChatClaude([...props.sessionChatClaude, newMessageClaude]);
-                                               props.setSessionChatGigachat([...props.sessionChatGigachat, newMessageGigachat]);
+                                               // props.setSessionChatClaude([...props.sessionChatClaude, newMessageClaude]);
+                                               // props.setSessionChatGigachat([...props.sessionChatGigachat, newMessageGigachat]);
                                            })
                                            .finally(() => {
                                                // props.setValue('');
