c96a90ae762cf7ffa82be6c8f30c491df15ea933
Новиков Алексей Юрьевич <novikov_ay@altravita.local>
2024-05-31 17:28:57 +0300

    new session110

--- src/components/firstChat/firstChat.jsx
+++ src/components/firstChat/firstChat.jsx
@@ -51,7 +51,8 @@ const FirstChat = (props) => {
                                                // props.llm_session_id = response.data.llm_sessions[0].id;
                                                // console.log(props.llm_session_id);
                                                props.setSession(response.data.llm_sessions);
-                                               props.llm_session_id = response.data.llm_session_id;
+                                               props.llm_session_id = response.data.id;
+                                               console.log(props.llm_session_id)
                                                // const messageGpt = response.data.filter(el => el.datetime_response === 'chatgpt');
                                                const messageGpt = response.data.llm_responses[0];
                                                const messageClaude = response.data.llm_responses[1];
