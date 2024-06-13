9d00dd9786e2517b04d027017ff11f04b1013134
noval000 <nowikov00@mail.ru>
2024-06-13 15:42:52 +0300

    input for change password12

--- src/components/changePassword/changePassword.jsx
+++ src/components/changePassword/changePassword.jsx
@@ -7,27 +7,27 @@ const ChangePassword = (props) => {
 
     const [axiosChangePassword, setAxiosChangePassword] = useState(false);       //      при изменении этого состояния отправляется форма (смена данных пользователя)
 
-    useEffect(() => {
-        const submitFirstChat = (e) => {
-            // Данные для отправки на сервер
-            const data = {
-                password
-            };
-            // Отправка данных на сервер
-            axios.post('/api/profile/change_profile_settings', data)
-                .then(response => {
-                    console.log('Server response:', response.data);
-
-                })
-                .finally(() => {
-
-                })
-                .catch(error => {
-                    console.error('There was an error sending the data!', error);
-                });
-        };
-        submitFirstChat();
-    }, [axiosChangePassword]);    //  отправка изменений профиля
+    // useEffect(() => {
+    //     const submitFirstChat = (e) => {
+    //         // Данные для отправки на сервер
+    //         const data = {
+    //             password
+    //         };
+    //         // Отправка данных на сервер
+    //         axios.post('/api/profile/change_profile_settings', data)
+    //             .then(response => {
+    //                 console.log('Server response:', response.data);
+    //
+    //             })
+    //             .finally(() => {
+    //
+    //             })
+    //             .catch(error => {
+    //                 console.error('There was an error sending the data!', error);
+    //             });
+    //     };
+    //     submitFirstChat();
+    // }, [axiosChangePassword]);    //  отправка изменений профиля
 
     return (
         <form action="">
